63802bf459ff4fe3896e7662fccdf8ab0921612a
David Ruttka <david@davidruttka.com>
2014-04-26 20:53:36 -0700

    Update assembly info and nuspec

--- owin/MadeWithLove.Middleware/Properties/AssemblyInfo.cs
+++ owin/MadeWithLove.Middleware/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9a4d9a41-547e-4994-8f79-582199ee2760")]
 
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.*")]
+[assembly: AssemblyVersion("0.2.1.*")]
+[assembly: AssemblyFileVersion("0.2.1.*")]
